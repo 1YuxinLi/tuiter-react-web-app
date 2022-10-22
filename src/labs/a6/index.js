ac6a47708df94fd41c067817e5ce5a5560d844f5
李雨芯 <liyuxin@liyuxindeMacBook-Pro.local>
2022-10-21 23:16:01 -0700

    a6 lab and tuiter

---
+++ src/labs/a6/index.js
@@ -0,0 +1,16 @@
+import Classes from "./classes";
+import Styles from "./styles";
+// import ConditionalOutput from "./conditional-output/index.js";
+import TodoList from "./todo/todo-list";
+
+function Assignment6() {
+    return (
+        <div>
+            <h1>Assignment 6</h1>
+            <Styles/>
+            <Classes/>
+            <TodoList/>
+        </div>
+    );
+}
+export default Assignment6;
