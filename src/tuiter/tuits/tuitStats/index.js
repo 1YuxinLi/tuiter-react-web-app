710331d21492147cb0888a8baadcae6a285a4784
yuxin li <yuxinli@yuxins-MacBook-Pro.local>
2022-11-18 11:25:45 -0800

    fix tuit stats

--- src/tuiter/tuits/tuitStats/index.js
+++ src/tuiter/tuits/tuitStats/index.js
@@ -1,69 +1,40 @@
-import React, {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {updateTuitThunk}
-    from "../../services/tuits-thunks";
-
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const dispatch = useDispatch();
-// eslint-disable-next-line react-hooks/rules-of-hooks
-useEffect(() => {
-    dispatch(updateTuitThunk())
-}, [])
-
-
+import React from "react";
+import {useDispatch} from "react-redux";
+import {updateTuitThunk} from "../../../services/tuits-thunks";
 const TuitStats = (
     {
-        tuit = { "_id": 123,
-            "topic": "Space",
-            "userName": "SpaceX",
-            "title": "Tesla Cybertruck lands on Mars and picks up the Curiosity rover on its 6' bed",
-            "time": " .2h",
-            "image": "starship.jpeg",
-            "liked": true,
-            "replies": 123,
-            "retuits": 432,
-            "likes": 2345,
-            "handle": " @spacex",
-            "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars"
-        }
+        post
     }
-
-) => {
-
+)=>{
+    const dispatch = useDispatch();
     return(
-        <div className="row">
-            <ul className="nav mb-2 ">
-                <li className="nav-item col-3">
-                    <a className="nav-link text-secondary" href="#/">
-                        <i className="bi bi-chat"></i> {tuit.replies}
-                    </a>
-                </li>
-                <li className="nav-item col-3">
-                    <a className="nav-link text-secondary" href="#/">
-                        <i className="bi bi-reply"></i> {tuit.replies}
-                    </a>
-                </li>
-                <li className="nav-item col-3">
+        <div className="wd-icons wd-nav-tags">
+            <ul className="list-inline row">
+                <li className="col flex-row "><i
+                    className="fa fa-comment-o wd-color-gray"></i><span
+                    className={`wd-color-gray ms-1`}>{post.comment}</span></li>
+                <li className="col flex-row ">
+                    <div>
+                        Likes: {post.likes}
+                        <i onClick={() => dispatch(updateTuitThunk({
+                            ...post,
+                            likes: post.likes + 1
+                        }))} className="bi bi-heart-fill me-2 text-danger ms-2"></i>
+                        <i onClick={() => dispatch(updateTuitThunk({
+                            ...post,
+                            likes: post.likes - 1
+                        }))} className="bi bi-hand-thumbs-down-fill me-2 text-danger"></i>
+                    </div>
 
-                    <a className="nav-link text-secondary fg-color-red" href="#/">
-                        <div>
-                            Likes: {tuit.likes}
-                            <i onClick={() => dispatch(updateTuitThunk({
-                                ...tuit,
-                                likes: tuit.likes + 1
-                            }))} className="bi bi-heart-fill me-2 text-danger"></i>
-                        </div>
-                    </a>
+
                 </li>
-                <li className="nav-item col-3">
-                    <a className="nav-link text-secondary" href="#/">
-                        <i className="bi bi-share"></i>
-                    </a>
-                </li>
+                <li className="col flex-row "><i
+                    className="fa fa-exchange wd-color-gray"></i><span
+                    className={`wd-color-gray ms-1`}>{post.transfer}</span></li>
+                <li className="col flex-row "><i
+                    className="fa fa-download wd-color-gray"></i></li>
             </ul>
         </div>
-
-    );
-
-
-}; export default TuitStats;
+    )
+};
+export default TuitStats;
