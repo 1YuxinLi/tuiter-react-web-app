141f794f636513f37acaa03e64d0eec7187026f3
yuxin li <yuxinli@yuxins-MacBook-Pro.local>
2022-11-23 14:36:30 -0800

    fix bug

--- src/tuiter/tuits/tuitStats/index.js
+++ src/tuiter/tuits/tuitStats/index.js
@@ -13,21 +13,34 @@ const TuitStats = (
                 <li className="col flex-row "><i
                     className="fa fa-comment-o wd-color-gray"></i><span
                     className={`wd-color-gray ms-1`}>{tuit.comment}</span></li>
-                <li className="col flex-row ">
-                    <div>
-                        Likes: {tuit.likes}
+                <li className="col flex-row text-secondary">
                         <i onClick={() => dispatch(updateTuitThunk({
                             ...tuit,
                             likes: tuit.likes + 1
-                        }))} className="bi bi-heart-fill me-2 text-danger ms-2"></i>
-                        <i onClick={() => dispatch(updateTuitThunk({
-                            ...tuit,
-                            likes: tuit.likes - 1
-                        }))} className="bi bi-hand-thumbs-down-fill me-2 text-danger"></i>
-                    </div>
-
+                        }))} className="bi bi-heart-fill me-2 text-danger ms-2"></i> {tuit.likes}
+                </li>
+                <li className="col flex-row text-secondary">
+                   <i onClick={() => dispatch(updateTuitThunk({
+                    ...tuit,
+                    likes: tuit.likes - 1
+                }))} className="bi bi-hand-thumbs-down-fill me-2 text-danger"></i> {tuit.likes}
 
                 </li>
+                <li className="nav-item col-3">
+                    <a className="nav-link text-secondary" href="#/">
+                        <i className="bi bi-chat"></i>
+                        <br/>
+                        {tuit.replies}
+                    </a>
+                </li>
+                <li className="nav-item col-3">
+                    <a className="nav-link text-secondary" href="#/">
+                        <i className="bi bi-reply"></i>
+                        <br/>
+                        {tuit.retuits}
+                    </a>
+                </li>
+
                 <li className="col flex-row "><i
                     className="fa fa-exchange wd-color-gray"></i><span
                     className={`wd-color-gray ms-1`}>{tuit.transfer}</span></li>
