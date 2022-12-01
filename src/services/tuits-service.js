61179952f6f466f7d616a30f6a1dda6003df9423
yuxin li <yuxinli@yuxins-MacBook-Pro.local>
2022-11-30 17:36:35 -0800

    fix bug

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const TUITS_API = 'https://tuiter-node-server-app-yuxin98.herokuapp.com/api/tuits';
+const TUITS_API = 'https://tuiter-node-server-app-yuxinli.herokuapp.com/api/tuits';
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
